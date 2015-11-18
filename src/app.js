d84195316e6f8c650386e2d786cab85a1fc7fb21
Andrew <expfront@gmail.com>
2015-11-18 23:26:59 +0300

    Updates

--- src/app.js
+++
@@ -1,7 +0,0 @@
-import './styles/bootstrap.min.css';
-import './styles/style.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import RootContainer from './containers/root';
-
-ReactDOM.render((<RootContainer />), document.getElementById(`app`));
